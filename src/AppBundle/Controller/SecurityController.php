cfcb65d06e85cce65ba86e8edef0aa8385b67b36
hundrex <kiruahxh32@yahoo.fr>
2018-03-08 18:41:13 +0100

    Ref #49: Add a forgotten use statement

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Account;
 
